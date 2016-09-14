b0b0bbee576e1593e6dea4f6951dd12f19aa28da
Placemaker <placemaker@qq.com>
2016-09-14 19:01:20 +0800

    learn python

---
+++ escapecharacter.py
@@ -0,0 +1,15 @@
+print('\\\t\\')
+print(r'\\\t\\')
+
+print('''happy
+coding
+hhh''')
+
+print('I\'m \"OK\"!')
+
+print('n =',123)
+print('f =',456.789)
+print('s1 =',"'Hello,world'")
+print('s2 =',r"'Hello,\'Adam\''")
+print('s3 =',"r\'Hello,\"Bart\"\'")
+print('s4 =','r\'\'\'Hello,\nLisa!\'\'\'')
